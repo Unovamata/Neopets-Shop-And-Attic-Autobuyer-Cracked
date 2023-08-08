a3e347e2d6b4a31868006a85cfc15e560cf7fd4b
Unovamata <badelgado@uned.cr>
2023-08-08 17:52:07 -0600

    Updates from Version 0.3.30 to 0.3.44
    • The new "inject.js" script from version 0.3.44 massively improves performance with less lines of code.
    • Some pages look different now for easier access.

---
+++ Autobuyer/src/inject/util.js
@@ -0,0 +1,6 @@
+function getParameterByName(e, n) {
+    n || (n = window.location.href), e = e.replace(/[[\]]/g, "\\$&");
+    var r = new RegExp("[?&]" + e + "(=([^&#]*)|&|#|$)")
+        .exec(n);
+    return r ? r[2] ? decodeURIComponent(r[2].replace(/\+/g, " ")) : "" : null
+}}
