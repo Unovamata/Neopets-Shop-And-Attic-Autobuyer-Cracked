7a70bfaea2a851c0266b522cc547e1bf71acd6f8
Unovamata <badelgado@uned.cr>
2023-08-12 18:02:36 -0600

    Update background.js
    • Added version 0.3.51 features.
    • Quick Stock management for auto DBing items bought.
    • Autorefresh when pages fail to load.
    • Turn on or off notifications.

--- Autobuyer/src/bg/background.js
+++ Autobuyer/src/bg/background.js
@@ -16,12 +16,11 @@ var extpay = ExtPay("restock-highligher-autobuyer");
 
 // Crack user;
 extpay.startBackground();
-extpay.getUser()
-	.then(user => {
-		chrome.storage.local.set({ EXT_P_S: true});
-	}).catch(error => {
-		console.error(error);
-	})
+extpay.getUser().then(user => {
+	chrome.storage.local.set({ EXT_P_S: true});
+}).catch(error => {
+	console.error(error);
+})
 	
 // Attic Captcha Warning after 3 seconds;
 setTimeout(() => {
@@ -40,4 +39,33 @@ chrome.action.onClicked.addListener(() => {
 
 chrome.runtime.onInstalled.addListener(function(e) {
 	"install" == e.reason || e.reason
+}), chrome.runtime.onMessage.addListener((function(e, t, o) {
+	t.url.indexOf("neopets.com") < 0 || "NeoBuyer" === e.neobuyer && ("Notification" != e.type ? "OpenQuickstockPage" != e.type ? "Beep" != e.type ? console.error("Received message with unknown purpose:", JSON.stringify(e)) : chrome.storage.local.get({
+		SHOULD_SOUND_ALERTS: !0
+	}, (function(e) {
+		e.SHOULD_SOUND_ALERTS
+	})) : chrome.tabs.create({
+		url: "https://www.neopets.com/quickstock.phtml?itemToQuickstock=" + e.itemName
+	}, (e => setTimeout((function() {
+		chrome.tabs.remove(e.id)
+	}), 8e3))) : chrome.storage.local.get({
+		SHOULD_SHOW_CHROME_NOTIFICATIONS: !0,
+		SHOULD_SOUND_ALERTS: !0
+	}, (function(t) {
+		t.SHOULD_SHOW_CHROME_NOTIFICATIONS && chrome.notifications.create(e.notificationObject), t.SHOULD_SOUND_ALERTS && chrome.tts.speak(e.notificationObject.title, {
+			rate: 1.15
+		})
+	})))
+})), chrome.webNavigation.onErrorOccurred.addListener((function(e) {
+	chrome.storage.local.get({
+		SHOULD_REFRESH_THROUGH_PAGE_LOAD_FAILURES: !0
+	}, (function(t) {
+		t.SHOULD_REFRESH_THROUGH_PAGE_LOAD_FAILURES && ("outermost_frame" !== e.frameType || "net::ERR_HTTP_RESPONSE_CODE_FAILURE" !== e.error || setTimeout((function() {
+			chrome.tabs.reload(e.tabId, (function() {}))
+		}), 1e4))
+	}))
+}), {
+	url: [{
+		urlMatches: "(.*neopets.com/halloween/garage.phtml)|(.*neopets.com/objects.phtml?(.)*type=shop(.)*)|(.*neopets.com/objects.phtml?(.)*obj_type=([0-9]+)(.)*)"
+	}]
 });
