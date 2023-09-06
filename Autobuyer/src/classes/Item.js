47cd90be29a8834edd9bcd126580b3d4f5fd5d1b
Unovamata <badelgado@uned.cr>
2023-09-06 17:16:36 -0600

    AutoPricer Pricer, Name, and Should Be Priced Data
    • Now the AutoPricer can load the player's shop shock, including prices, if it should be priced, and names.
    • The AutoPricer table can now be sorted.
    • Some CSS changes.

---
+++ Autobuyer/src/classes/Item.js
@@ -0,0 +1,11 @@
+//######################################################################################################################################
+
+class Item {
+    constructor(Name, Price, IsPricing){
+        this.Name = Name;
+        this.Price = Price;
+        this.IsPricing = IsPricing;
+    }
+}
+
+module.exports = Item;
--- Autobuyer/src/options/Autopricer/autopricer.js
+++ Autobuyer/src/options/Autopricer/autopricer.js
@@ -4,14 +4,68 @@ function setSHOP_INVENTORY(value) {
 
 var inventoryData = [];
 
-chrome.storage.local.get(['SHOP_INVENTORY'], function (result) {
-    inventoryData = result.SHOP_INVENTORY;
+const table = document.getElementById("shop-inventory");
 
-    console.log(inventoryData);
+function ReadInventoryData(){
+    chrome.storage.local.get(['SHOP_INVENTORY'], function (result) {
+        inventoryData = result.SHOP_INVENTORY;
+    
+        console.log(inventoryData);
+    
+        inventoryData.forEach( Item => {
+    
+            var row = table.insertRow();
+    
+            var cellName = row.insertCell(0);
+            cellName.innerHTML = Item.Name;
+    
+            var cellPrice = row.insertCell(1);
+            var priceInput = document.createElement("input");
+            priceInput.value = Item.Price;
+    
+            cellPrice.appendChild(priceInput);
+    
+            var cellShouldPrice = row.insertCell(2);
+            var shouldPriceInput = document.createElement("input");
+            shouldPriceInput.type = "checkbox";
+            shouldPriceInput.checked = Item.IsPricing;
+    
+            cellShouldPrice.appendChild(shouldPriceInput);
+            
+            // Add an event listener to the checkbox for real-time updates
+            shouldPriceInput.addEventListener("change", function () {
+                if (shouldPriceInput.checked) {
+                    row.classList.add("checked-row");
+                } else {
+                    row.classList.remove("checked-row");
+                }
+            });
 
-    //Update the history data every 5 seconds;
-    ProcessPurchaseHistory(false), setInterval((function() {
-        ProcessPurchaseHistory(false)
-    }), 5e3)
-});
+            //console.log(Item.Name);
 
+            // Add class to the row based on the checkbox value
+            if (Item.IsPricing) {
+                row.classList.add("checked-row");
+            }
+        });
+    });
+
+    table.classList.add("sortable")
+
+    MakeSortableTable();
+}
+
+function MakeSortableTable(){
+    // Loop through all the table elements in the document
+    forEach(document.getElementsByTagName("table"), function(tableElement) {
+        // Find sortable elements and make them sortable;
+        if (tableElement.className.search(/\bsortable\b/) !== -1) {
+            sorttable.makeSortable(tableElement);
+            tableElement.classList.add("table");
+        }
+    });
+}
+
+ReadInventoryData();
+
+//setInterval(ReadInventoryData, 5000);
