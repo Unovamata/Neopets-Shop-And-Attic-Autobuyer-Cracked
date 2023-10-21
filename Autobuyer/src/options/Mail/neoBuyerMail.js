4eea88f4b31729bb0eff1315f930a844245b1cff
Unovamata <badelgado@uned.cr>
2023-10-20 20:50:26 -0600

    NeoBuyer+ Mail Improvements
    • Now the user can delete emails coming from the NeoBuyer+ developer.
    • Mails can not be sorted by entry, allowing the navigation and read of old mails in the system.
    • A user cannot receive mails with the same ID.
    • The mails are inserted in the GUI if saved, in a table.
    • The GUI automatically hides and shows the inbox or mail window depending on what the user wants to do.

--- Autobuyer/src/options/Mail/neoBuyerMail.js
+++ Autobuyer/src/options/Mail/neoBuyerMail.js
@@ -1,7 +1,8 @@
 const emailURL = "https://raw.githubusercontent.com/Unovamata/Neopets-Shop-And-Attic-Autobuyer-Cracked/main/Autobuyer/src/options/Mail/MailDocument.html"; // Replace with the URL you want to ping
 
 class Email {
-    constructor(ID, Author, Date, Subject, Contents){
+    constructor(Entry, ID, Author, Date, Subject, Contents){
+        this.Entry = Entry;
         this.ID = ID;
         this.Author = Author;
         this.Date = Date;
@@ -10,36 +11,73 @@ class Email {
     }
 }
 
+function getEMAIL_LIST(callback) {
+    chrome.storage.local.get(['EMAIL_LIST'], function (result) {
+        var value = result.EMAIL_LIST;
+
+        if(value == undefined) value = [];
+
+        if (typeof callback === 'function') {
+            callback(value);
+        }
+    });
+}
+
+function setEMAIL_LIST(value) {
+    chrome.storage.local.set({ EMAIL_LIST: value }, function () {});
+}
 
 fetch(emailURL)
   .then(response => response.text())
   .then(htmlContent => {
+    const parser = new DOMParser();
+    const githubDocument = parser.parseFromString(htmlContent, 'text/html');
+
     // 'html' contains the HTML content of the site
     console.log(htmlContent);
 
-    var ID = document.getElementById("id");
-    var author = document.getElementById("author");
-    var date = document.getElementById("date");
-    var subject = document.getElementById("subject");
-    var contents = document.getElementById("contents");
+    var ID = githubDocument.getElementById("id").textContent;
+    var author = githubDocument.getElementById("author").textContent;
+    var date = githubDocument.getElementById("date").textContent;
+    var subject = githubDocument.getElementById("subject").textContent;
+    var contents = githubDocument.getElementById("contents").textContent;
 
-    //message.innerHTML = htmlContent;
+    var extractedEmail = new Email(0, ID, author, date, subject, contents);
 
-    var testEmail = new Email(ID, author, date, subject, contents);
-    
-    InsertNewEmailRow(testEmail);
+    getEMAIL_LIST(function (emailList){
+        console.log(emailList);
+
+        for(var i = 0; i < emailList.length; i++){
+            var email = emailList[i];
+            email.Entry = i + 1;
+            InsertNewEmailRow(email);
+        }
+
+        const hasEmail = [...emailList].some(email => email.ID == ID);
+
+        if(hasEmail){
+            return;
+        }
+
+        emailList.unshift(extractedEmail);
+        setEMAIL_LIST(emailList);
+    });
+
+    var loading = document.getElementById("loading-messages");
+    loading.style.visibility = "hidden";
 
 }).catch(error => {
-    console.error("Error:", error);
+    console.error("An error ocurred during the execution... Try again later...", error);
 });
 
-function InsertNewEmailRow(email){
-    var inbox = document.getElementById("inbox");
+var inbox = document.getElementById("inbox");
+var activeEmail= null;
 
+function InsertNewEmailRow(email){
     var newEmailRow = inbox.insertRow();
 
-    var emailIDCell = newEmailRow.insertCell(0);
-    emailIDCell.textContent = email.ID;
+    var emailEntryCell = newEmailRow.insertCell(0);
+    emailEntryCell.textContent = email.Entry;
 
     var emailAuthorCell = newEmailRow.insertCell(1);
     emailAuthorCell.textContent = email.Author;
@@ -51,7 +89,54 @@ function InsertNewEmailRow(email){
     emailSubjectCell.textContent = email.Subject;
 
     var emailReadCell = newEmailRow.insertCell(4);
-    emailReadCell.textContent = "Read";
+
+    // Adding the envelope icon;
+    var aElement = document.createElement('a');
+    var imgElement = document.createElement('img');
+    imgElement.src = "../../toolbar/neomail.svg";
+    imgElement.classList.add("mail-inbox");
+
+    aElement.appendChild(imgElement);
+    emailReadCell.appendChild(aElement);
+    emailReadCell.style.cursor = "pointer";
+
+    emailReadCell.addEventListener("click", function(event){
+        var cellIndex = Number(newEmailRow.querySelector("td:first-child").textContent);
+
+        getEMAIL_LIST(function (emailList){
+            activeEmail = emailList[cellIndex - 1];
+
+            inbox.style.visibility = "hidden";
+            messageContainer.style.visibility = "visible";
+            returnToInboxButton.style.visibility = "visible";
+            messageContainer.textContent = activeEmail.Contents;
+        });
+    });
 
     inbox.appendChild(newEmailRow);
+}
+
+var messageContainer = document.getElementById("message");
+messageContainer.style.visibility = "hidden";
+
+var returnToInboxButton = document.getElementById("return-to-inbox");
+
+returnToInboxButton.addEventListener("click", ShowInbox);
+
+ShowInbox();
+
+function ShowInbox(){
+    messageContainer.style.visibility = "hidden";
+    messageContainer.textContent = "";
+    inbox.style.visibility = "visible";
+    returnToInboxButton.style.visibility = "hidden";
+}
+
+var deleteEmailsButton = document.getElementById("reset");
+deleteEmailsButton.addEventListener("click", DeleteMails);
+
+function DeleteMails(){
+    setEMAIL_LIST([]);
+    window.alert("All NeoBuyer+ mails have been successfully deleted!");
+    window.location.reload();
 }
